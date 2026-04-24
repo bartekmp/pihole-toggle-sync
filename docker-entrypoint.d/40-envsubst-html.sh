26d8bf78cfd7337af98f20e1d42c04dc472d25eb
Bartosz Pollok <bartekmp@users.noreply.github.com>
2026-04-24 16:00:27 +0200

    feature: indefinitely disable blocking, version in the footer (#2)

--- docker-entrypoint.d/40-envsubst-html.sh
+++ docker-entrypoint.d/40-envsubst-html.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-envsubst '${PH_HOSTS} ${PH_PASSWORD}' \
+envsubst '${PH_HOSTS} ${PH_PASSWORD} ${APP_VERSION}' \
     < /usr/share/nginx/html/index.html.template \
     > /usr/share/nginx/html/index.html
